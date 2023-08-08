91f2f09ce574be07532495ef3336967ea9a083b1
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-08-08 18:59:34 +0300

    all

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import Icon from '@mdi/react';
 import { mdiPlus } from '@mdi/js';
 
-
 const AddButton=()=>{
     return (       
         <Link  className={styles.addButton} to="event">                    
